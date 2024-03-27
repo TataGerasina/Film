63b10784c6382b3bc0673ef17c4198bbaa7f0d44
Tatyana Gerasina <gtstata@gmail.com>
2024-03-27 14:31:38 +0300

    changes have been made to the project

--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -42,8 +42,6 @@ public class FilmManagerTest {
         manager.add("Отель Белград");
         manager.add("Джентельмены");
 
-
-
         String[] expected = {"Бладшот", "Вперёд", "Отель Белград", "Джентельмены"};
         String[] actual = manager.findAll();
 
